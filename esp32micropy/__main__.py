f31a0c860f165d205f8a824448827890979d59b6
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-03-11 17:52:40 -0400

    Update __main__.py

--- esp32micropy/__main__.py
+++ esp32micropy/__main__.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from warnings import warn, Warning
+import urllib.request
 
 try:
     os.system('ampy')
@@ -19,8 +20,29 @@ print('Checked for dependencies.')
 if "--bootfile" in sys.argv:
     tempoargs = sys.argv
     tempoargs.remove('--bootfile')
-    print('Putting {} on ESP32 memory'.format(tempoargs[1]))
-    os.system('ampy {} main.py'.format(tempoargs[1]))
-    print('Put the file on memory')
-elif "--":
-   ...
+    print('Putting {} on ESP32 memory as the boot file'.format(tempoargs[1]))
+    os.system('ampy {} put {} main.py'.format(tempoargs[1],tempoargs[2]))
+    print('Put the file on memory!')
+elif "--copy" in sys.argv":
+   tempoargs = sys.argv
+   tempoargs.remove('--copy')
+   print('Putting {} on ESP32 memory'.format(tempoargs[1]))
+   os.system('ampy {} put {} {}'.format(tempoargs[1], tempoargs[2],tempoargs[3]))
+   print('Put that on memory!')
+elif "--get" in sys.argv:
+    tempoargs = sys.argv
+    tempoargs.remove('--get')
+    print('Getting that file')
+    out = os.system('ampy {} get {}'.format(tempoargs[1],tempoargs[2]))
+    print(out)
+else:
+    print('Installing MicroPython')
+    f = input('Please Confirm. Yes: 1. No: 0')
+    if int(f) == 1:
+        print('Installing MicroPython... Do not unplug your board')
+        print('Erasing Flash... Do not unplug your board')
+        os.system('esptool.py --port {} erase_flash'.format(sys.argv[1]))
+        print('Erased Flash. Downloading MicroPython... Do not unplug your board')
+        
+        
+                  
