8b02eb3c587ac97e52f0845a16d31f60ec6c8482
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-03-15 17:16:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'esp32micropy',         # How you named your package folder (MyLib)
   packages = ['esp32micropy'],   # Chose the same as "name"
-  version = '1.3',      # Start with a small number and increase it with every change you make
+  version = '1.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Tool for wrking with and installing MicroPython',   # Give a short description about your library
   author = 'JonathanG',                   # Type in your name
   author_email = 'ceo@graffbt.com',      # Type in your E-Mail
   url = 'https://github.com/zurgeg/esp32-micropython-installer',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/zurgeg/security-python/master/v1_3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/zurgeg/security-python/master/v1_4.tar.gz',    # I explain this later on
   keywords = ['ESP', 'Espressif', 'ESP32','MicroPython'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'adafruit-ampy',
