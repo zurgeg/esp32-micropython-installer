2ca5d72d031176da3627717d740b57e589e7b8be
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-03-15 17:16:22 -0400

    Update __main__.py

--- esp32micropy/__main__.py
+++ esp32micropy/__main__.py
@@ -20,19 +20,19 @@ if "--bootfile" in sys.argv:
     tempoargs = sys.argv
     tempoargs.remove('--bootfile')
     print('Putting {} on ESP32 memory as the boot file'.format(tempoargs[1]))
-    os.system('ampy {} put {} main.py'.format(tempoargs[1],tempoargs[2]))
+    os.system('ampy -p {} put {} main.py'.format(tempoargs[1],tempoargs[2]))
     print('Put the file on memory!')
 elif "--copy" in sys.argv:
    tempoargs = sys.argv
    tempoargs.remove('--copy')
-   print('Putting {} on ESP32 memory'.format(tempoargs[1]))
-   os.system('ampy {} put {} {}'.format(tempoargs[1], tempoargs[2],tempoargs[3]))
+   print('Putting {} on ESP32 memory'.format(tempoargs[2]))
+   os.system('ampy -p {} put {} {}'.format(tempoargs[1], tempoargs[2],tempoargs[3]))
    print('Put that on memory!')
 elif "--get" in sys.argv:
     tempoargs = sys.argv
     tempoargs.remove('--get')
     print('Getting that file')
-    out = os.system('ampy {} get {}'.format(tempoargs[1],tempoargs[2]))
+    out = os.system('ampy -p {} get {}'.format(tempoargs[1],tempoargs[2]))
     print(out)
 elif "--serverhost" in sys.argv:
     tempoargs = sys.argv
@@ -42,7 +42,7 @@ elif "--serverhost" in sys.argv:
     while True:
         f = open(tempoargs[1] + '/main.py')
         if f.readlines() == prevf:
-            os.system('ampy {} put {} main.py'.format(tempoargs[2],tempoargs[1]+'/main.py'))
+            os.system('ampy -p {} put {} main.py'.format(tempoargs[2],tempoargs[1]+'/main.py'))
             prevf = f.readlines()
         else:
             #No Changes!
@@ -50,18 +50,21 @@ elif "--serverhost" in sys.argv:
 elif "--test" in sys.argv:
     os.system('ampy --help')
     os.system('esptool.py --help')
+    print(sys.argv)
+    print(sys.argv[1])
 else:
     print('Installing MicroPython')
     f = input('Please Confirm. Yes: 1. No: 0')
     if int(f) == 1:
         print('Installing MicroPython... Do not unplug your board')
         print('Erasing Flash... Do not unplug your board')
-        p1 = os.system('esptool.py --port {} erase_flash'.format(sys.argv[2]))
+        p1 = os.system('esptool.py --port {} erase_flash'.format(sys.argv[1]))
         print('Erased Flash. Downloading MicroPython... Do not unplug your board')
         url = 'http://micropython.org/resources/firmware/esp32-idf3-20191220-v1.12.bin'
         urllib.request.urlretrieve(url, 'micropython.bin')
         print('MicroPython Version: IDF3, Year: 2019, Month: 12, Day: 20')
         print('Downloaded MicroPython. Flashing MicroPython... DO NOT UNPLUG YOUR BOARD!')
-        p2 = os.system('esptool.py --port {} --baud 460800 write_flash --flash_size=detect 0 micropython.bin'.format(sys.argv[1]))
+        p2 = os.system('esptool.py --port {} --baud 460800 write_flash --flash_size=detect -z 0x1000 micropython.bin'.format(sys.argv[1]))
         
                   
+
